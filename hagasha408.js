c4d78a269b8431c74e1c2e5c4bbdc487a7dba006
barikassa <barikassa01@gmail.com>
2021-08-07 14:48:19 +0300

    first commit

---
+++ hagasha408.js
@@ -0,0 +1,139 @@
+// //1.
+// var array = [7, 9, 4, 17, 46, 2], minAge = array[0], maxAge = array[0], sum = 0;
+
+// for (var  i = 1; i < array.length; i++) {
+//     if (array[i] < minAge) {
+//         minAge = array[i]
+
+//     }
+// }
+// console.log(minAge);
+
+// for( var i=1; i<Array.length; i++ ){
+//     if(array[i]>maxAge){
+//     maxage!= Array[i];
+//     }
+// }
+// console.log(maxAge);
+
+//2. 
+// var x = ["lali", 7, 900, "dani", "dondon"], yname=[], ynum=[];
+// console.log(x[i]);
+
+// for (var i = 0; i < x.length; i++) {
+//     if (Number(x[i])) {
+//           ynum.push(x[i])
+//     }
+//     else{
+//         yname.push(x[i])
+//     }
+
+// }
+// console.log(yname);
+// console.log(ynum);
+//3. 
+
+// var x=[5,7,6,4,6,7,8,9,], y=x[0], z=x[1];
+// if(x[1]=x[0]){
+//  console.log(x);
+//  if(z=x){
+//      console.log(x);
+//  }
+
+// }
+
+//4. 
+// var x=[5,3,7,8,11], sum=0;
+// for(var d=0; d<x.length; d++){
+//    sum +=x[d];
+//    console.log(sum);
+//    console.log(sum/x.length);
+// }
+
+//5.
+// var x= ["dabi", "dada","kaka","nana", "lala"] ;
+// for(d=0; d<x.length;d++){
+//     console.log(x);
+// }
+//6. 
+// var x=[], userInput;
+
+// for(d=0 ; d<5; d++){
+//     userInput=prompt("name");
+// x.push(userInput);
+
+// }
+//7. 
+
+// var x= ["dabi", "chaim","kaka","nana", "lala"], userInput ;
+
+// for(c=0; c<5;c++){
+//     userInput=prompt("name");
+//  if(x[c]===userInput){
+ 
+//  }
+//     else {
+//         console.log("wrong name");
+//     }
+// }
+// console.log("good");
+//8. 
+// var y=[], userInput, multi=1, sum;
+// for(var c=0; c<3; c++){
+//     userInput=Number(prompt("you"));
+//     y.push(userInput);
+//     multi=y[c]*y.length;
+//     sum =sum+y[c];
+// }
+
+// console.log(y);
+// console.log(multi);
+// console.log(sum);
+//9. 
+// var arrayA =[1,2,3,4,5,6,7,8,9,10], arrayB=[1,2,3,4,5,6,7,8,9,10], multiA;
+// for(var i=0; i<arrayA.length;i++){
+//     arrayA =[1,2,3,4,5,6,7,8,9,10]
+//     multiA= arrayA[i]
+//     console.log(multiA);
+//     for(var j=0; j<arrayB.length; j++){
+//         arrayB=[1,2,3,4,5,6,7,8,9,10]
+//         multiB= arrayB[j]*arrayA[i]
+//         console.log(multiB);
+
+//     }
+// }
+    //10. 
+    // var n=["a", "b", "c", "d"],userInput, num=[];
+    //   for(var i=0; i<n.length; i++){
+    //     userInput=(prompt("number"));
+    //     num.push(userInput);
+    //     // multi=num.length*n
+        
+    //   }
+
+    //   console.log(n);
+
+    //11. 
+    // var arrayInput=[], userInput;
+    
+    // for( var i=0;i <=3; i++){
+    //     userInput=Number(prompt("play the game"))
+    //     if(userInput != 7){
+    //         console.log("keep playing")
+    //     }
+           
+    //     else if(userInput==7 || userInput+10==x ){
+           
+    //         console.log("boom");
+    //     }
+
+    // }
+
+    
+
+
+
+
+
+
+
